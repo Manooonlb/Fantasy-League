bed59d3d6bd3fc22c142ea549d60f1eb7d3efa0d
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-03-15 15:29:13 +0100

    Ajout de la fonctionnalité de modification de la langue fr / en

--- nav.php
+++ nav.php
@@ -1,33 +1,20 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <!-- Container wrapper -->
   <div class="container">
-    <!-- Navbar brand -->
-    <a class="navbar-brand me-2" href="">
-      <img src="https://www.jimdo.com/static/77d4f7715e8aed2ca34dc4e31a50f182/d8516/logo-dummy-fr.png" height="16" alt="Fantasy League Logo" loading="lazy" style="margin-top: -1px;" />
-    </a>
-
     <!-- Toggle button -->
     <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation">
       <i class="fas fa-bars"></i>
     </button>
-
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarButtonsExample">
       <!-- Left links -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">FR</a>
+          <a onclick="setLanguage('fr')" class="nav-link" href="#">FR</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">EN</a>
+          <a onclick="setLanguage('en')" class="nav-link" href="#">EN</a>
         </li>
       </ul>
-      <!-- Left links -->
-      <button onclick="window.location.href='profil.php'" class="btn btn-light">Profil</button>
     </div>
-    <!-- Collapsible wrapper -->
   </div>
-  <!-- Container wrapper -->
 </nav>
 <!-- Navbar -->

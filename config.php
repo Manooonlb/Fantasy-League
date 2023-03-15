e35729b3c8ae26c72629bf88b8eb7ce9003cb8f0
OceaneCha <oceane.chabrerie.33@gmail.com>
2023-03-15 13:41:03 +0100

    add login.php, config.php initialized database co

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'fantasy_league');
 
-$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+/* $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if (!$connection) {
     die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
-}
+} */

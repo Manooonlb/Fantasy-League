3c59c8fc56fe6fe77e27e1755ffa7dee18c1c81d
Sofia Slimani <[mmeslimani.s@gmail.com] (mailto:mmeslimani.s@gmail.com)>
2023-03-15 16:46:32 +0100

    Co-authored-by: amon <ZinaSzt@users.noreply.github.com>

--- footer.php
+++ footer.php
@@ -1,89 +1,44 @@
 <!-- Footer -->
 <footer class="text-center text-lg-start bg-dark text-muted">
-  <!-- Section: Social media -->
-  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    <!-- Left -->
-    <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <!-- Left -->
-
-    <!-- Right -->
-    <div>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
-    </div>
-    <!-- Right -->
-  </section>
+ <!-- Section: Social media -->
+  
   <!-- Section: Social media -->
 
   <!-- Section: Links  -->
-  <section class="">
+  <section class="text-center">
+    <img class="d-inline-flex p-2 d-flex align-items-center"  src="./assets/wild_code_school.png" alt="wcs_logo">
     <div class="container text-center text-md-start mt-5">
       <!-- Grid row -->
-      <div class="row mt-3">
+      <div class="d-flex justify-content-evenly">
         <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+       
           <!-- HOME -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>HOME
+           <a href="" class="link-light">HOME</a>
           </h6>
           
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+      
           <!-- CONDITIONS -->
           <h6 class="text-uppercase fw-bold mb-4">
-            CONDITIONS D'UTILISATION
+           <a href="" class="link-light"> CONDITIONS D'UTILISATION</a>
           </h6>
           
           
-        </div>
+        
         <!-- Grid column -->
 
         <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
           <!-- FAQ -->
           <h6 class="text-uppercase fw-bold mb-4">
-            FAQ
+            <a href="" class="link-light">FAQ</a>
           </h6>
           
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- CONTACT-->
-          <h6 class="text-uppercase fw-bold mb-4">CONTACT</h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-          <p>
-            <i class="fas fa-envelope me-3"></i>
-            info@example.com
-          </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
-        </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
+          <h6 class="text-uppercase fw-bold mb-4"><a href="" class="link-light">CONTACT</h6></a> 
+          
+      
+</div>
     </div>
   </section>
   <!-- Section: Links  -->

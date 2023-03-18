089b885802f5b8a3f116ef7b76a538a390c4f604
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-03-18 11:46:05 +0100

    Modification mise en forme code nav

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div class="container">
-      <!-- Left links -->
-      <?php if (!isset($_SESSION['username'])) { ?>
+    <!-- Left links -->
+    <?php if (!isset($_SESSION['username'])) { ?>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a onclick=" lang('fr')" class="nav-link" href="?lang=fr">FR</a>
@@ -11,18 +11,17 @@
           <a onclick="setLanguage('en')" class="nav-link" href="?lang=en">EN</a>
         </li>
       </ul>
-      <?php }
-      else { ?>
+    <?php } else { ?>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#"><?=TXT_NAV_LEAGUES?></a>
+          <a class="nav-link" href="#"><?= TXT_NAV_LEAGUES ?></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><?=TXT_NAV_MY_PROFILE?></a>
+          <a class="nav-link" href="#"><?= TXT_NAV_MY_PROFILE ?></a>
         </li>
       </ul>
-      <?php }?>
-    </div>
+    <?php } ?>
+  </div>
   </div>
 </nav>
 <!-- Navbar -->

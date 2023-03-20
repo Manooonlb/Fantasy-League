ca11e3696a12ae2f753b6e306061e22cbdde84a5
Sofia Slimani <[mmeslimani.s@gmail.com] (mailto:mmeslimani.s@gmail.com)>
2023-03-21 00:06:48 +0100

    ..

--- thanks.php
+++ thanks.php
@@ -1,8 +1,10 @@
 <?php
-$username = $_POST['username'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+if (isset($_POST['username'], $_POST['email'], $_POST['message'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-echo "Merci $username de nous avoir contacté !<br><br>";
-echo "Nous t'apporterons une réponse à l’adresse $email dans les plus brefs délais :<br><br>";
-echo "$message";
+    echo "Merci $username de nous avoir contacté !<br><br>";
+    echo "Nous t'apporterons une réponse à l’adresse $email dans les plus brefs délais :<br><br>";
+    echo "$message";
+}

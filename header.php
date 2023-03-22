18a0f8deba76e659f4f500f5eb166e5b18bbebf6
Sofia Slimani <[mmeslimani.s@gmail.com] (mailto:mmeslimani.s@gmail.com)>
2023-03-22 16:53:11 +0100

    changed the layout of the contact form and added and alert message

--- header.php
+++ header.php
@@ -6,42 +6,40 @@ session_start(); // Init session
 // --------------------------------
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($_POST['login'] === 'true') {
-        $_SESSION['username'] = $_POST['username'];
-        $_SESSION['email'] = $_POST['userEmail'];
-        $_SESSION['favoriteGame'] = $_POST['favoriteGame'];
-    }
-    if ($_POST['login'] === 'logout') {
-        session_destroy();
+    if (isset($_POST['login'])) {
+        if ($_POST['login'] === 'true') {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['email'] = $_POST['userEmail'];
+            $_SESSION['favoriteGame'] = $_POST['favoriteGame'];
+        }
+        if ($_POST['login'] === 'logout') {
+            session_destroy();
+        }
     }
 }
 
 // --------------------------------
 // Set language preferences and call language constants
 // --------------------------------
-if(isset($_GET['lang'])){
-    $lang=$_GET['lang'];
-}
-elseif(isset($_COOKIE['lang'])){
-    $lang=$_COOKIE['lang'];
-}
-else{
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+} elseif (isset($_COOKIE['lang'])) {
+    $lang = $_COOKIE['lang'];
+} else {
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 }
 
-if ($lang =='fr'){
+if ($lang == 'fr') {
     include_once('language/fr_lang.php');
-}
-else if ($lang=='en'){
+} else if ($lang == 'en') {
     include_once('language/en_lang.php');
-}
-else{
+} else {
     include_once('language/fr_lang.php');
 }
 
 // Set a cookie for the language
-$expire= 365*24*3600;
-setcookie('lang', $lang, time() +$expire);
+$expire = 365 * 24 * 3600;
+setcookie('lang', $lang, time() + $expire);
 ?>
 
 <!DOCTYPE html>
@@ -58,10 +56,8 @@ setcookie('lang', $lang, time() +$expire);
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
 
 
-     <link href="css/style.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
 
-     <link rel="icon" type="image/x-icon" href="/assets/favicon.png" />
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png" />
 
 </head>
-
-

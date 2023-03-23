4ef91f948d404a7b81002e527d153136e42b5de9
ZinaSzt <iukwim404@gmail.com>
2023-03-23 10:51:15 +0100

    finalisation FAQ

--- faq.php
+++ faq.php
@@ -6,19 +6,19 @@ require_once('header.php');
   <?php
   include_once('nav.php');
   ?>
-
-  <div class="accordion" id="accordionFAQ">
+  <main class="d-flex justify-content-center">
+  <div class="accordion accordion-flush bg-dark accordion-borderless w-50 my-2" id="accordionFAQ">
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingOne">
-        <button class="accordion-button" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseOne"
+        <button class="accordion-button accordion-flush bg-dark text-white-50 bg-dark" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseOne"
           aria-expanded="true" aria-controls="collapseOne">
           FAQ #1
         </button>
       </h2>
       <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
         data-mdb-parent="#accordionFAQ">
-        <div class="accordion-body">
-          <strong>This is the first faq question.</strong> 
+        <div class="accordion-body bg-secondary bg-gradient">
+          <strong>This is the First faq question.</strong> 
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Maxime earum at tenetur omnis itaque modi. Totam iusto deserunt sapiente, impedit,
           itaque cum amet id rerum inventore numquam ullam quaerat eos! 
@@ -28,15 +28,15 @@ require_once('header.php');
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingTwo">
-        <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        <button class="accordion-button collapsed accordion-flush bg-dark text-white-50 bg-dark" type="button" data-mdb-toggle="collapse"
           data-mdb-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
           FAQ #2
         </button>
       </h2>
       <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
         data-mdb-parent="#accordionFAQ">
-        <div class="accordion-body">
-        <strong>This is the second faq question.</strong> 
+        <div class="accordion-body bg-secondary bg-gradient">
+        <strong>This is the Second faq question.</strong> 
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Maxime earum at tenetur omnis itaque modi. Totam iusto deserunt sapiente, impedit,
           itaque cum amet id rerum inventore numquam ullam quaerat eos! 
@@ -46,15 +46,51 @@ require_once('header.php');
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingThree">
-        <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+        <button class="accordion-button collapsed accordion-flush bg-dark text-white-50 bg-dark" type="button" data-mdb-toggle="collapse"
           data-mdb-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
           FAQ #3
         </button>
       </h2>
       <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
         data-mdb-parent="#accordionFAQ">
-        <div class="accordion-body">
-        <strong>This is the third faq question.</strong> 
+        <div class="accordion-body bg-secondary bg-gradient">
+        <strong>This is the Third faq question.</strong> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Maxime earum at tenetur omnis itaque modi. Totam iusto deserunt sapiente, impedit,
+          itaque cum amet id rerum inventore numquam ullam quaerat eos! 
+          <strong>.ceci est un test</strong>.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingFour">
+        <button class="accordion-button collapsed accordion-flush bg-dark text-white-50 bg-dark" type="button" data-mdb-toggle="collapse"
+          data-mdb-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          FAQ #4
+        </button>
+      </h2>
+      <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+        data-mdb-parent="#accordionFAQ">
+        <div class="accordion-body bg-secondary bg-gradient">
+        <strong>This is the Fourth faq question.</strong> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Maxime earum at tenetur omnis itaque modi. Totam iusto deserunt sapiente, impedit,
+          itaque cum amet id rerum inventore numquam ullam quaerat eos! 
+          <strong>.ceci est un test</strong>.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingFive">
+        <button class="accordion-button collapsed accordion-flush bg-dark text-white-50 bg-dark" type="button" data-mdb-toggle="collapse"
+          data-mdb-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          FAQ #5
+        </button>
+      </h2>
+      <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive"
+        data-mdb-parent="#accordionFAQ">
+        <div class="accordion-body bg-secondary bg-gradient">
+        <strong>This is the Fifth faq question.</strong> 
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Maxime earum at tenetur omnis itaque modi. Totam iusto deserunt sapiente, impedit,
           itaque cum amet id rerum inventore numquam ullam quaerat eos! 
@@ -63,7 +99,9 @@ require_once('header.php');
       </div>
     </div>
   </div>
+  </main>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
   <?php
   include_once('footer.php');
   ?>
+</body>

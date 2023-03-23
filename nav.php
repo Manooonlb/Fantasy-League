b01a7507e5b526a9320c55054c194c90e5d5cba8
Sofia Slimani <[mmeslimani.s@gmail.com] (mailto:mmeslimani.s@gmail.com)>
2023-03-23 10:45:34 +0100

    nav logout

--- nav.php
+++ nav.php
@@ -6,19 +6,7 @@
         <?php if (!isset($_SESSION['username'])) {
         ?>
 
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                <a onclick=" lang('fr')" class="nav-link" href="?lang=fr">FR</a>
-            </li>
-            <li class="nav-item">
-                <a onclick="setLanguage('en')" class="nav-link" href="?lang=en">EN</a>
-            </li>
-        </ul>
-        <?php } else {
-        ?>
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
                 <li class="nav-item">
                     <a onclick=" lang('fr')" class="nav-link" href="?lang=fr">FR</a>
                 </li>
@@ -26,47 +14,57 @@
                     <a onclick="setLanguage('en')" class="nav-link" href="?lang=en">EN</a>
                 </li>
             </ul>
+        <?php } else {
+        ?>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <div class="container justify-content-center">
-                <img class="py-3 w-25" src="assets\fantasyLeague2_trans.png" alt="<?= TXT_LOGO_ALT ?>" />
-            </div>
+                    <li class="nav-item">
+                        <a onclick=" lang('fr')" class="nav-link" href="?lang=fr">FR</a>
+                    </li>
+                    <li class="nav-item">
+                        <a onclick="setLanguage('en')" class="nav-link" href="?lang=en">EN</a>
+                    </li>
+                </ul>
 
-            <!-- Right elements -->
-            <div class="">
+                <div class="container justify-content-center">
+                    <img class="py-3 w-25" src="assets\fantasyLeague2_trans.png" alt="<?= TXT_LOGO_ALT ?>" />
+                </div>
 
-                <!-- Notifications -->
-                <img src="assets/pink_diamond.png" height="25">
-                <span class="badge">1524</span>
-                <img src="assets/money.png" height="25">
-                <span class="badge">80</span>
+                <!-- Right elements -->
+                <div class="">
 
-                <!-- Avatar -->
-                <div class="dropdown">
-                    <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
-                        id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" height="25"
-                            alt="Black and White Portrait of a Man" loading="lazy" />
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-                        <li>
-                            <a class="dropdown-item" href="#">My profile</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#"><?= TXT_NAV_MY_LEAGUES ?></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#"><?= TXT_NAV_MY_STATS ?></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#"><?= TXT_NAV_MY_REWARDS ?></a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#"><?= TXT_NAV_MY_FRIENDS ?></a>
-                        </li>
+                    <!-- Notifications -->
+                    <img src="assets/pink_diamond.png" height="25">
+                    <span class="badge">1524</span>
+                    <img src="assets/money.png" height="25">
+                    <span class="badge">80</span>
 
-                    </ul>
+                    <!-- Avatar -->
+                    <div class="dropdown">
+                        <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#" id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" height="25" alt="Black and White Portrait of a Man" loading="lazy" />
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                            <li>
+                                <a class="dropdown-item" href="#">My profile</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"><?= TXT_NAV_MY_LEAGUES ?></a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"><?= TXT_NAV_MY_STATS ?></a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"><?= TXT_NAV_MY_REWARDS ?></a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"><?= TXT_NAV_MY_FRIENDS ?></a>
+                            </li>
 
-        </ul>
+                        </ul>
+
+            </ul>
     </div>
     </div>
 
@@ -81,6 +79,6 @@
 
     </div>
 
-    <?php } ?>
-    </div>
+<?php } ?>
+</div>
 </nav>
